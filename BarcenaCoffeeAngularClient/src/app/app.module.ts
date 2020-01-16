07c43b165efdefc7f6e997acaac137c18a2b1110
Joshua Barcena <Alycse@gmail.com>
2020-01-16 20:55:08 +0800

    Add Angular components

--- BarcenaCoffeeAngularClient/src/app/app.module.ts
+++ BarcenaCoffeeAngularClient/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    NotFoundComponent,
+    OrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'order', component: OrderComponent },
+      { path: '404', component : NotFoundComponent},
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', redirectTo: '/404', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
